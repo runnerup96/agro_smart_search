1fd6ca9f318f43070e071877285fb0c07960576c
Darby <dimanata1232@gmail.com>
2023-05-20 17:57:57 +0300

    added new features and search scores, designg changed

--- webapp/app.py
+++ webapp/app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
 import json
-from finder_class import Finder, sample_object
+from finder_class import Finder
 
 
 with open("/app/argo_smart_search/webapp/data/featureset.json", "rb") as file:
@@ -14,35 +14,48 @@ st.header("Поисковик по лекарственным растениям
 st.markdown("Преставленное решение разработано командой cls_token, в рамках кейса **ООО «СОЛЮШН»** хакатона \"Цифровой прорыв. Сезон: ИИ\"")
 st.subheader("👨‍🌾 Сформируйте запрос")
 
-
-options = st.multiselect(
+location_feature = st.multiselect(
     "Расположение (GEO)",
     input_data["location_feature"],
     [], max_selections=5)
-
-options1 = st.selectbox(
+climate_feature = st.selectbox(
     'Климат',
     input_data["climate_feature"],
     )
-options2 = st.selectbox(
+soil = st.selectbox(
     'Почва',
-    input_data["pochva_feature"],
+    input_data["soil"],
     )
-options3 = st.multiselect(
+chemicals_feature = st.multiselect(
     'Химическое вещество в растении',
     input_data["chemicals_feature"],
     [], max_selections=5)
-options4 = st.checkbox('Охраняется красной книгой')
+source_type = st.selectbox(
+    'Вид сырья',
+    input_data["source_type"],
+    )
+calendar_month = st.multiselect(
+    'Календарь сбора, мес.',
+    input_data["calendar_month"],
+    [], max_selections=5)
+max_type = st.multiselect(
+    'Максимальный срок хранения, год',
+    input_data["max_type"],
+    [], max_selections=5)
+red_book_feature = st.checkbox('Охраняется красной книгой')
 
 find_button = st.button("Найти")
 
 
 
-entity_attribures = {"location_feature": list(options),
-                     "climate_feature": list(options1),
-                     "red_book_feature": "1" if options4 else  [],
-                     "chemicals_feature": list(options3),
-                     "pochva_feature": list(options2)}
+entity_attribures = {"location_feature": list(location_feature),
+                     "climate_feature": list(climate_feature),
+                     "red_book_feature": "1" if red_book_feature else [],
+                     "chemicals_feature": list(chemicals_feature),
+                     "soil": list(soil),
+                     "source_type": list(source_type),
+                     "calendar_month": list(calendar_month),
+                     "max_type": list(max_type)}
 st.divider()
 if find_button:
     st.subheader("📈 Результат")
@@ -51,24 +64,31 @@ if find_button:
     for x in result:
         with st.expander(x["original_name"]):
             st.write("Атрибуты документа")
+            st.markdown("**Метрика схожести документа и запроса:  " + str(round(x["search_score"], 2))+"**")
 
             col1, col2, col3, col4, col5 = st.columns(5)
+            col6, col7, col8 = st.columns(3)
 
             with col1:
                 st.table(pd.DataFrame(x["entity_attributes"]["location_feature"],columns=["location_feature"]))
             with col2:
                 st.table(pd.DataFrame(x["entity_attributes"]["climate_feature"], columns=["climate_feature"]))
             with col3:
-                st.table(pd.DataFrame(x["entity_attributes"]["pochva_feature"], columns=["pochva_feature"]))
+                st.table(pd.DataFrame(x["entity_attributes"]["soil"], columns=["soil"]))
             with col4:
                 st.table(pd.DataFrame(x["entity_attributes"]["chemicals_feature"], columns=["chemicals_feature"]))
             with col5:
-                st.table( pd.DataFrame(x["entity_attributes"]["red_book_feature"], columns=["red_book_feature"]))
+                st.table(pd.DataFrame(x["entity_attributes"]["red_book_feature"], columns=["red_book_feature"]))
+            with col6:
+                st.table(pd.DataFrame(x["entity_attributes"]["calendar_month"], columns=["calendar_month"]))
+            with col7:
+                st.table(pd.DataFrame(x["entity_attributes"]["max_type"], columns=["max_type"]))
+            with col8:
+                st.table(pd.DataFrame(x["entity_attributes"]["calendar_month"], columns=["calendar_month"]))
+
             st.caption(x["entity_definition"])
 else:
     st.write("Чтобы начать поиск, нажмите \"Найти\"")
-
-
 
 st.caption("Powered by cls_token")
 
